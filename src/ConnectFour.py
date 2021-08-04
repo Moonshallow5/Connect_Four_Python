2b47c799354927c811821b289cabb40ec191e0b8
Sandeep Singh <ssandeepssingh55@gmail.com>
2021-08-04 23:18:39 +0800

    adf

--- src/ConnectFour.py
+++ src/ConnectFour.py
@@ -1,1 +1,132 @@
-print("Yo")
+class Model:
+    the_board = {'7': ' ', '8': ' ', '9': ' ',
+                 '4': ' ', '5': ' ', '6': ' ',
+                 '1': ' ', '2': ' ', '3': ' '}
+    yi = [[' ', ' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' ', ' ']]
+
+
+player = 'X'
+
+model = Model()
+
+
+def is_move_valid(c):
+    if c > 7 or c < 1:
+        print("Enter value in range")
+        return False
+    if model.yi[0][c - 1] != ' ':
+        return False
+
+    else:
+        return True
+
+
+def board():
+    for i in range(len(model.yi)):
+        for j in range(len(model.yi[i])):
+            print(model.yi[i][j], end=" ")
+        print()
+
+
+def is_wi_horizontal():
+    for i in model.yi:
+        rowCount = 0
+        for j in i:
+            if (j == player):
+                rowCount += 1
+            else:
+                rowCount = 0
+            if rowCount >= 3:
+                return True
+
+
+def is_win_vertical():
+    for col in range(len(model.yi[0])):
+        colCount=0
+        for chars in model.yi:
+            if(chars[col]==player):
+                colCount+=1
+            else:
+                colCount=0
+            if colCount>=3:
+                return True
+
+
+def yo(x):
+    while not is_move_valid(x):
+        x = int(input("Enter jnjn" + str(player) + ": "))
+        is_move_valid(x)
+    for row in range(len(model.yi) - 1, -1, -1):
+
+        if model.yi[row][x - 1] == ' ':
+            model.yi[row][x - 1] = player
+            printsss()
+            break
+
+
+def labels():
+    for i in range(7):
+        print(end="  ")
+        print('0' + str(i + 1), end="  ")
+
+
+def win():
+    i = 0
+    printsss()
+    while i == 0:
+        x = int(input("Enter " + str(player) + ": "))
+        yo(x)
+        if (is_wi_horizontal()):
+            break
+        if(is_win_vertical()):
+            break
+
+        switch_players()
+
+
+def printsss():
+    rowDivider = "--------------------------------------------"
+    print(rowDivider)
+    for i in range(len(model.yi)):
+        print("| ", end=" ")
+        for j in range(len(model.yi[0])):
+            print(model.yi[i][j], end=" ")
+            print(" | ", end=" ")
+
+        print()
+        print(rowDivider)
+
+
+def print_board(board):
+    print('-------------')
+    print('| ' + board['7'] + ' | ' + board['8'] + ' | ' + board['9'] + ' |')
+    print('-------------')
+
+    print()
+    print('| ' + board['4'] + ' | ' + board['5'] + ' | ' + board['6'] + ' |')
+    print('-------------')
+
+    print()
+    print('| ' + board['1'] + ' | ' + board['2'] + ' | ' + board['3'] + ' |')
+    print('-------------')
+    labels()
+    print()
+
+
+def switch_players():
+    global player
+    if player == 'X':
+        player = 'O'
+    elif player == 'O':
+        player = 'X'
+
+
+win()
+
+printsss()
+labels()
---
+++ src/TicTacToe.py
@@ -0,0 +1,220 @@
+def print_board(board):
+    print('-------------')
+    print('| ' + board['7'] + ' | ' + board['8'] + ' | ' + board['9'] + ' |')
+    print('-------------')
+    labels3()
+    print()
+    print('| ' + board['4'] + ' | ' + board['5'] + ' | ' + board['6'] + ' |')
+    print('-------------')
+
+    labels2()
+    print()
+    print('| ' + board['1'] + ' | ' + board['2'] + ' | ' + board['3'] + ' |')
+    print('-------------')
+
+    labels()
+    print()
+
+
+def is_move_valid(x):
+    if x > 9 or x <= 0:
+        print("Please enter value in range")
+        return False
+    elif model.the_board['{}'.format(x)] == 'X' or model.the_board['{}'.format(x)] == 'O':
+        print("Board is full")
+        return False
+    else:
+        return True
+
+
+class Model:
+    the_board = {'7': ' ', '8': ' ', '9': ' ',
+                 '4': ' ', '5': ' ', '6': ' ',
+                 '1': ' ', '2': ' ', '3': ' '}
+
+
+model = Model()
+
+
+def labels3():
+    for i in range(6, 9, 1):
+        print(end="  ")
+        print(str(i + 1), end=" ")
+
+
+def labels():
+    for i in range(3):
+        print(end="  ")
+        print(str(i + 1), end=" ")
+
+
+def labels2():
+    for i in range(3, 6, 1):
+        print(end="  ")
+        print(str(i + 1), end=" ")
+
+
+player = 'X'
+
+
+def switch_players():
+    global player
+
+    if player == 'X':
+        player = 'O'
+    elif player == 'O':
+        player = 'X'
+
+
+def is_winner_upward_diagonal():
+    if model.the_board['1'] == player and model.the_board['5'] == player and model.the_board['9'] == player:
+        return True
+
+
+def is_winner_vertical():
+    count = 0
+    ss1 = 0
+    kl = 1
+    while ss1 <= 2:
+        for i in range(kl, 10, 3):
+            if model.the_board['{}'.format(i)] == player:
+                count += 1
+                if count >= 3:
+                    return True
+        ss1 += 1
+        count = 0
+        kl += 1
+
+
+def is_winner_horizontal():
+    count = 0
+    ss1 = 0
+    kl = 1
+    while ss1 <= 2:
+        for i in range(kl, kl + 3, 1):
+
+            if model.the_board['{}'.format(i)] == player:
+                count += 1
+
+                if count >= 3:
+                    return True
+        ss1 += 1
+        count = 0
+        kl += 3
+
+
+def is_winner_downward_diagonal():
+    if model.the_board['7'] == player and model.the_board['5'] == player and model.the_board['3'] == player:
+        return True
+
+
+wonX = 0
+wonO = 0
+
+
+def concede(x):
+    global wonX, wonO
+    if x == -1:
+        switch_players()
+        print(str(player) + " wins")
+        if player == 'X':
+            wonX += 1
+        elif player == 'O':
+            wonO += 1
+        print("Score is " + str(wonX) + " to X and " + str(wonO) + " to O")
+        return True
+
+
+def some():
+    global wonX
+    global wonO
+    print(str(player) + " wins")
+    if is_winner_upward_diagonal():
+        print("Due to upward diagonal win")
+    elif is_winner_horizontal():
+        print("Due to horizontal win")
+    elif is_winner_vertical():
+        print("Due to vertical win")
+    elif is_winner_downward_diagonal():
+        print("Due to downward diagonal win")
+
+    if player == 'X':
+        wonX += 1
+
+    elif player == 'O':
+        wonO += 1
+    print("Score is " + str(wonX) + " to X and " + str(wonO) + " to O")
+
+
+def play_again():
+    print("Would you like to play again Enter Y to play again")
+    y = input("Enter: ")
+    if y == "Y":
+        model.the_board['1'] = ' '
+        model.the_board['2'] = ' '
+        model.the_board['3'] = ' '
+        model.the_board['4'] = ' '
+        model.the_board['5'] = ' '
+        model.the_board['6'] = ' '
+        model.the_board['7'] = ' '
+        model.the_board['8'] = ' '
+        model.the_board['9'] = ' '
+        print_board(model.the_board)
+        if player == 'O':
+            switch_players()
+        game()
+
+
+def win():
+    count = 0
+    i = 0
+    global ss
+    ss = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    while i == 0:
+        print("Available moves: " + str(ss))
+        x = int(input("Enter" + str(player) + ": "))
+        if x != -1:
+            yo(x)
+            count += 1
+            print_board(model.the_board)
+
+            print()
+            if is_winner_vertical():
+                some()
+                break
+            elif is_winner_horizontal():
+                some()
+                break
+            elif is_winner_upward_diagonal():
+                some()
+                break
+            elif is_winner_downward_diagonal():
+                some()
+                break
+            if count == 9:
+                print("Tie Game")
+                break
+
+            switch_players()
+        else:
+            concede(x)
+            break
+
+
+def yo(x):
+    while not is_move_valid(x):
+        print("Try again")
+        x = int(input("Enter " + str(player) + ": "))
+        is_move_valid(x)
+    ss.remove(x)
+    model.the_board['{}'.format(x)] = player
+
+
+def game():
+    win()
+    play_again()
+
+
+print("Enter -1 to concede")
+print_board(model.the_board)
+game()
